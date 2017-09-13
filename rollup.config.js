3515bb1b736141676598e7bfa45a0d48985bfaba
Aakash N S <aakashns.sky@gmail.com>
2017-09-13 07:13:08 +0530

    uglify umd build

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
+import uglify from "rollup-plugin-uglify";
 import pkg from "./package.json";
 
 export default [
@@ -14,7 +15,8 @@ export default [
       commonjs(),
       babel({
         exclude: ["node_modules/**"]
-      })
+      }),
+      uglify()
     ]
   },
   {
